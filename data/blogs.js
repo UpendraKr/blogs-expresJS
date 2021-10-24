f96b70c2b88a9bd346737d8064eb681195889f5d
Upendra Kumar <upendrakumarvipin@gmail.com>
2021-10-24 23:30:27 +0530

    blogs

---
+++ data/blogs.js
@@ -0,0 +1,21 @@
+blogs = [
+    {
+        "title" : "How to get started with Python",
+        "content": "This is content for python.",
+        "slug": "Python-learn"
+    },
+    {
+        "title" : "How to get started with JS",
+        "content": "This is content for JS.",
+        "slug": "JS-learn"
+    },
+    {
+        "title" : "How to get started with JAVA",
+        "content": "This is content for JAVA.",
+        "slug": "JAVA-learn"
+    }
+
+
+]
+
+module.exports = blogs
--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const path = require("path")
+const myblogs = require("../data/blogs")
 
 
 router.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '../templates/index.html'))
+    // res.sendFile(path.join(__dirname, '../templates/index.html'))
+    res.render('./home');
 })
 
+router.get('/blog', (req, res) =>{
+    myblogs.forEach(e => {
+        console.log(e.title)
+    });
+    // res.sendFile(path.join(__dirname, '../templates/blogHome.html'))
+    res.render('./blogHome', {
+        blogs: myblogs
+    });
+})
+
+router.get('/blogpost/:slug', (req, res) =>{
+    blog = myblogs.filter((e)=>{
+        return e.slug == req.params.slug
+    })
+    // console.log(blog)
+    // res.sendFile(path.join(__dirname, '../templates/blogPage.html'))
+    res.render('./blogPage', {
+        title: blog[0].title,
+        content: blog[0].content
+    });
+})
 
 module.exports = router
